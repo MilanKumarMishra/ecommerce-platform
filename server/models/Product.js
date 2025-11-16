3bd7bb790814fbdefb4fcfe39c2ca08a47a20d49
MilanKumarMishra <milankrmishra@gmail.com>
2025-11-16 21:11:05 +0530

    Add missing Product model

---
+++ server/models/Product.js
@@ -0,0 +1,12 @@
+// server/models/Product.js
+const mongoose = require('mongoose');
+
+const productSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  price: { type: Number, required: true },
+  description: { type: String },
+  image: { type: String },
+  category: { type: String }
+});
+
+module.exports = mongoose.model('Product', productSchema);
